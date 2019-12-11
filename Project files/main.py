9aa7767b7a9b5f8b18b2bc309996e692f2a38402
Pyry Vaara <vaara.pyry@gmail.com>
2019-12-11 21:37:29 +0200

    start of main()

--- Project files/main.py
+++ Project files/main.py
@@ -1,2 +1,12 @@
+# This is Particle Interaction Simulator 0.1 by Pyry Vaara.
+# It is a work in progress
+
 def main():
-    print()
+
+    print("Hello and welcome to Particle Interaction Simulation 0.1.")
+    print("What would you like to do? Enter your choice \n")
+    print("")
+
+
+
+main()
